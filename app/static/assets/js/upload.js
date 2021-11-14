c204e3e868dce790c7a268c43e0ec783ee07de4e
César Medrano <cesar.galvanmedrano@gmail.com>
2021-11-13 19:53:23 -0600

    Subir archivo y mejoras visuales
    Se agrega maqueta para subir imagen y validaciones, logo del Tec, icono de flor

---
+++ app/static/assets/js/upload.js
@@ -0,0 +1,69 @@
+$(function () {
+    $('#profile').addClass('dragging').removeClass('dragging');
+});
+
+$('#profile').on('dragover', function () {
+    $('#profile').addClass('dragging');
+}).on('dragleave', function () {
+    $('#profile').removeClass('dragging');
+}).on('drop', function (e) {
+    $('#profile').removeClass('dragging hasImage');
+
+    if (e.originalEvent) {
+        var file = e.originalEvent.dataTransfer.files[0];
+        var fileName = file.name;
+        var idxDot = fileName.lastIndexOf(".") + 1;
+        var extFile = fileName.substr(idxDot, fileName.length).toLowerCase();
+
+        if (extFile == "jpg" || extFile == "jpeg" || extFile == "png") {
+            var reader = new FileReader();
+            
+            reader.readAsDataURL(file);
+            reader.onload = function (e) {
+                console.log(reader.result);
+                $('#profile').css('background-image', 'url(' + reader.result + ')').addClass('hasImage');
+            }
+        } else {
+            alert("¡Solamente estan permitidos archivos jpg/jpeg y png!");
+        }
+        
+
+    }
+})
+$('#profile').on('click', function (e) {
+    console.log('clicked')
+    $('#mediaFile').click();
+});
+window.addEventListener("dragover", function (e) {
+    e = e || event;
+    e.preventDefault();
+}, false);
+window.addEventListener("drop", function (e) {
+    e = e || event;
+    e.preventDefault();
+}, false);
+
+$('#mediaFile').on("change", function (e) {
+    var input = e.target;
+
+    if (input.files && input.files[0]) {
+        var file = input.files[0];
+        var fileName = file.name;
+        var idxDot = fileName.lastIndexOf(".") + 1;
+        var extFile = fileName.substr(idxDot, fileName.length).toLowerCase();
+        
+        if (extFile == "jpg" || extFile == "jpeg" || extFile == "png") {
+            var file = input.files[0];
+            var reader = new FileReader();
+
+            reader.readAsDataURL(file);
+            reader.onload = function (e) {
+                console.log(reader.result);
+                $('#profile').css('background-image', 'url(' + reader.result + ')').addClass('hasImage');
+            }
+        } else {
+            alert("¡Solamente estan permitidos archivos jpg/jpeg y png!");
+        }
+        
+    }
+});
