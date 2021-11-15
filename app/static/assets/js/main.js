ceb1187252753e0dc726222f2a6247f1cc7ad7c6
César Medrano <cesar.galvanmedrano@gmail.com>
2021-11-14 22:39:40 -0600

    Mostrar password
    Mejora en input de password

---
+++ app/static/assets/js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+    $("#show-password button").on('click', function (e) {
+        e.preventDefault();
+        if ($('#show-password input').attr("type") == "password") {
+            $('#show-password input').attr('type', 'text');
+            $('#show-password i').addClass("bi-eye-slash-fill");
+            $('#show-password i').removeClass("bi-eye-fill");
+        } else {
+            $('#show-password input').attr('type', 'password');
+            $('#show-password i').removeClass("bi-eye-slash-fill");
+            $('#show-password i').addClass("bi-eye-fill");
+        }
+    });
+});
