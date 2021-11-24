d95a7a6da09dd6192f0432a6791b85daf4b5b3dc
César Medrano <cesar.galvanmedrano@gmail.com>
2021-11-23 21:30:27 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from app import create_app
 
 if __name__ == "__main__":
-    app = create_app
-    app.run(debug=True)
+    app = create_app()
+    app.run(debug=False, host="0.0.0.0", port="8083")
